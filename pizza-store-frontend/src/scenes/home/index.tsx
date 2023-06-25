d466a27caa5f5f5b29f0527293d858767325c86c
Samuel Wemimo <wemsamuel@gmail.com>
2023-06-25 14:49:38 -0700

    update: home page completed

--- pizza-store-frontend/src/scenes/home/index.tsx
+++ pizza-store-frontend/src/scenes/home/index.tsx
@@ -1,45 +1,72 @@
-import useMediaQuery from '@/hooks/useMediaQuery';
-import ActionButton from '@/shared/ActionButton';
 import { SelectedPage } from '@/shared/types';
 import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
-// const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
 
 const Home = ({ setSelectedPage }: Props) => {
   return (
-    <section id="home" className="bg-gray-20 gap-16 py-10 md:h-full md:pb-0">
-      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
-        <div>
-          <div>
-            <img alt="home-page-text" src={HomePageText} />
-          </div>
-          <p>
-            Indulge in the Irresistible Delights of Crusty Sam's Pizzeria: Where
-            Every Slice is a Savory Symphony of Handcrafted Crust and
-            Mouthwatering Toppings, Bringing You the Ultimate Pizza Experience!
-          </p>
-        </div>
-        <div>
-          <ActionButton setSelectedPage={setSelectedPage}>
-            Join Now!
-          </ActionButton>
-          <AnchorLink
-            className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-            href={`#${SelectedPage.ContactUs}`}
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        <div className="z-10 mt-32 md:basis-3/5">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
-            <p>Learn About Us!</p>
-          </AnchorLink>
-          <div>
-            <img alt="home-page-graphic" src={HomePageGraphic} />
-          </div>
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm">
+              Indulge in the Irresistible Delights of Crusty Sam's Pizzeria:
+              Where Every Slice is a Savory Symphony of Handcrafted Crust and
+              Mouthwatering Toppings, Bringing You the Ultimate Pizza
+              Experience!
+            </p>
+          </motion.div>
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
         </div>
-      </div>
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
     </section>
   );
 };
