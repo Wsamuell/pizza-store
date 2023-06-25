421cd4117954636defa85d37c87d6bd7964c751c
Samuel Wemimo <wemsamuel@gmail.com>
2023-06-25 02:56:01 -0700

    update: wip nav bar incomplete styling

--- pizza-store-frontend/src/scenes/navbar/index.tsx
+++ pizza-store-frontend/src/scenes/navbar/index.tsx
@@ -1,29 +1,111 @@
-import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import { SelectedPage } from '@/shared/types';
+
 import Logo from '@/assets/Logo.png';
+import Link from './Link';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { useState } from 'react';
+import ActionButton from '@/shared/ActionButton';
+// import ActionButton from '@/shared/ActionButton';
 
-type Props = {};
+type Props = {
+  isTopOfPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const NavBar = (props: Props) => {
+const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = 'flex items-center justify-between';
+  const flexBottom = 'flex items-end justify-between';
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const navbarBackground = isTopOfPage ? '' : 'bg-primary-100 drop-shadow';
+
   return (
     <nav>
-      <div className={`${flexBetween} top-0 z-30 flex w-full py-6`}>
-        <div className={`${flexBetween} mx-auto w-5/6`}></div>
-        <div className={`${flexBetween} w-full gap-16`}>
-          <div className={`${flexBetween} gap-8 text-sm`}>
-            <p>Home</p>
-            <p>Our Specials</p>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 flex w-full items-center py-6`}
+      >
+        {/* <div className={`${flexBetween} w-full gap-16`}></div> */}
+        {isAboveMediumScreens ? (
+          // <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBottom}`}>
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Specials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
           </div>
-          <img alt="logo" src={Logo} className=" w-44" />
-          <div className={`${flexBetween} w-full`}>
-            <div className={`${flexBetween} gap-8 text-sm`}>
-              <p>Log In</p>
-              <p>Contact Us!</p>
+        ) : (
+          // </div>
+          <div />
+        )}
+        <img alt="logo" src={Logo} className=" w-64" />
+
+        {isAboveMediumScreens ? (
+          <div className={`${flexBetween} w-full gap-16`}>
+            <div className={`${flexBetween} gap-8`}>
+              <Link
+                page="Contact Us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Log In
+              </ActionButton>
+              {/* <Link
+                  page="Log In"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                /> */}
             </div>
           </div>
-        </div>
+        ) : (
+          <button
+            className="rounded-full bg-secondary-400 p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <Bars3Icon className="h-6 w-6 text-white" />
+          </button>
+        )}
       </div>
+
+      {/* {mobile menu} */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Specials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            {/* <ActionButton setSelectedPage={setSelectedPage}>
+              Log In
+            </ActionButton> */}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
---
+++ pizza-store-frontend/src/shared/types.ts
@@ -0,0 +1,6 @@
+export enum SelectedPage {
+  Home = 'home',
+  Specials = 'specials',
+  LogIn = 'login',
+  ContactUs = 'contactus',
+}
